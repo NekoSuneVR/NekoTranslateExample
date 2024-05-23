cbf03171b1ad546f4b43726306b62b2c191dec8b
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-05-23 01:05:38 +0100

    Add files via upload

---
+++ Test Translator/Program.cs
@@ -0,0 +1,29 @@
+﻿using NekoTranslate;
+using System.Text.Json;
+
+namespace TranslatorApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var source = "en";
+            var translate = "pl";
+            var text = "hello, how are you?";
+            var provider = "google";
+
+            // Translate text from English to German using Google Translate
+            var translatedText = await Translator.Translate(source, translate, text, provider);
+
+            // Parse JSON and extract the translation
+            var jsonDocument = JsonDocument.Parse(translatedText);
+            var transNode = jsonDocument.RootElement.GetProperty("sentences")[0].GetProperty("trans");
+            var translation = transNode.GetString();
+
+            // Output the translated text
+            Console.WriteLine("provider: " + provider);
+            Console.WriteLine("Translate: " + source + " -> " + translate);
+            Console.WriteLine("Text: " + text + " -> " + translation);
+        }
+    }
+}
